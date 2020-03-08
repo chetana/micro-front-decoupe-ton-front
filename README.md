d242154352ca63fc16fabaf2fdd59ea13950ef83
Chetana <cyin@e-biz.fr>
2020-03-08 17:35:52 +0100

    step-0

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# micro-front-decoupe-ton-front
+# Microdex - Le micro pokedex
+
+## Step 0 - Installation et Lancement de l'application
+
+### Step 0.1 - Installer npm
+
+#### Debian / Ubuntu
+
+```
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+```
+
+### Step 0.2 - Installer http-server
+
+####
+```
+sudo npm install http-server -g
+```
+
+### Step 0.3 - Lancer l'application
+
+```
+http-server
+```
+
+L'application est ici : <http://localhost:8080/>
+
+## Passer à l'étape 1
+```
+git checkout step-1
+```
---
+++ main.js
@@ -0,0 +1,58 @@
+const rootList = document.getElementsByClassName("pokemon-list")[0];
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+};
+let addNameToPokemonElement = function(pokeElement, pokemon) {
+    let nameElement = document.createElement("div");
+    nameElement.setAttribute("class", "pokemon-title");
+    nameElement.innerHTML = pokemon.name;
+    pokeElement.append(nameElement);
+};
+let addTypeToPokemonElement = function(pokeElement, pokemon) {
+    if (pokemon.types.length > 0) {
+        let typesListElement = document.createElement("div");
+        typesListElement.setAttribute("class", "background-wrapper");
+        let backgroundType = document.createElement("div");
+        backgroundType.setAttribute("style", "background-color:"+colors[pokemon.types[0].type.name]+"");
+        typesListElement.append(backgroundType);
+        pokeElement.append(typesListElement);
+    }
+};
+
+let addImageToPokemonElement = function(pokeElement, pokemon) {
+    let imageElement = document.createElement("div");
+    imageElement.setAttribute("class", "image-wrapper");
+    let image = document.createElement("img");
+    image.setAttribute("src", pokemon.sprites.front_default);
+    image.setAttribute("class", "pokemon-image");
+    image.setAttribute("alt", pokemon.name);
+    imageElement.append(image);
+    pokeElement.append(imageElement);
+};
+
+let addPokemonToList = function(pokemon){
+    let pokeElement = document.createElement("div");
+    pokeElement.setAttribute("class", "pokemon-card");
+    doOnPokemon(pokemon.name, function(pokemonDetail) {
+        addTypeToPokemonElement(pokeElement, pokemonDetail);
+        addImageToPokemonElement(pokeElement, pokemonDetail);
+        addNameToPokemonElement(pokeElement, pokemonDetail);
+    });
+    rootList.append(pokeElement);
+};
+
+doOnAllPokemon(addPokemonToList);
+
